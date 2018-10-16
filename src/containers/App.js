34f4842236c509daed645a16a0c4f1e672d9ad67
Egor Sidorov <egorkqq@gmail.com>
2018-10-16 13:16:06 +0500

    Feat: moved App to containers folder

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,33 +1,30 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { User } from '../components/User'
+import { Page } from '../components/Page'
+
 import './App.css'
 
 class App extends Component {
   render() {
-    const { name } = this.props.user
-    const { year, photos } = this.props.page
+    const { user, page } = this.props
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Мой топ фото</h1>
         </header>
-        <p>Привет, {name}!</p>
-        <p>
-          У тебя {photos.length} фото за {year} год
-        </p>
+        <User name={user.name} />
+        <Page photos={page.photos} year={page.year} />
       </div>
     )
   }
 }
 
-// приклеиваем данные из store
 const mapStateToProps = store => {
-  console.log(store) // посмотрим, что же у нас в store?
   return {
     user: store.user,
     page: store.page,
   }
 }
 
-// в наш компонент App, с помощью connect(mapStateToProps)
 export default connect(mapStateToProps)(App)
