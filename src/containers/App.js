aa9ba88cd9be1e77bbfd825ca9ad1f8170d036ae
egorkqq <egorkqq@gmail.com>
2018-10-31 12:49:01 +0500

    feat: add styles, logics, reducers, vk integration etc

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,36 +2,46 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { User } from '../components/User'
 import { Page } from '../components/Page'
-import { setYear } from '../actions/PageActions'
-
-import './App.css'
+import { getPhotos } from '../actions/PageActions'
+import { handleLogin } from '../actions/UserActions'
 
 class App extends Component {
   render() {
-    const { user, page, setYearAction } = this.props
+    const { user, page, getPhotosAction, handleLoginAction } = this.props
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Мой топ фото</h1>
-        </header>
-        <User name={user.name} />
-        <Page photos={page.photos} year={page.year} setYear={setYearAction} />
+      <div className="app">
+        <Page
+          photos={page.photos}
+          year={page.year}
+          error={page.error}
+          isFetching={page.isFetching}
+          getPhotos={getPhotosAction}
+        />
+        <User
+          name={user.name}
+          isFetching={user.isFetching}
+          error={user.error}
+          handleLogin={handleLoginAction}
+        />
       </div>
     )
   }
 }
 
 const mapStateToProps = store => {
-  console.log(store)
   return {
     user: store.user,
     page: store.page,
   }
 }
 
-const mapDispatchToProps = dispatch => ({
-  setYearAction: year => dispatch(setYear(year)),
-})
+const mapDispatchToProps = dispatch => {
+  return {
+    getPhotosAction: year => dispatch(getPhotos(year)),
+
+    handleLoginAction: () => dispatch(handleLogin()),
+  }
+}
 
 export default connect(
   mapStateToProps,
