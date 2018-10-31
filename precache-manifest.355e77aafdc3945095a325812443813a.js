20429fdfa18ba1395afc19be86f5359455544462
egorkqq <egorkqq@gmail.com>
2018-10-31 15:06:37 +0500

    Updates

---
+++ precache-manifest.355e77aafdc3945095a325812443813a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "716dac67377b7e952a58",
+    "url": "/VKPhoto/static/js/runtime~main.716dac67.js"
+  },
+  {
+    "revision": "b8f8e9880d894e9a8f00",
+    "url": "/VKPhoto/static/js/main.b8f8e988.chunk.js"
+  },
+  {
+    "revision": "b0f1e4667a6147612831",
+    "url": "/VKPhoto/static/js/1.b0f1e466.chunk.js"
+  },
+  {
+    "revision": "b8f8e9880d894e9a8f00",
+    "url": "/VKPhoto/static/css/main.6b5c03d5.chunk.css"
+  },
+  {
+    "revision": "aa25f866545a54625b12588ac0b9bf12",
+    "url": "/VKPhoto/index.html"
+  }
+];
